9f7f57fe3547c5c1f6b4edfb2ff91b5f266fbef0
agoesm <muhammad.agus.miftah@gmail.com>
2020-09-23 16:06:44 +0700

    tambah id_logistik

--- app/Http/Controllers/LogistikController.php
+++ app/Http/Controllers/LogistikController.php
@@ -13,7 +13,7 @@ class LogistikController extends Controller
         $logistik=DB::table('t_logistik')
         ->join('t_kategori_logistik','t_logistik.id_kategori','=','t_kategori_logistik.id')
         ->join('t_supplier','t_logistik.id_supplier','=','t_supplier.id')
-        ->select('t_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
+        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
         ->orderByDesc('id_logistik')
         ->paginate(3);
         
@@ -24,7 +24,7 @@ class LogistikController extends Controller
         $logistik=DB::table('t_logistik')
         ->join('t_kategori_logistik','t_logistik.id_kategori','=','t_kategori_logistik.id')
         ->join('t_supplier','t_logistik.id_supplier','=','t_supplier.id')
-        ->select('t_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
+        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
         ->where('t_logistik.id_logistik', '=', $id)
         ->get();
         
@@ -58,6 +58,7 @@ class LogistikController extends Controller
         $logistik=DB::table('t_logistik')
         ->where('id_logistik', $id)
         ->update([
+            'id_logistik' => $request->id_logistik,
             'id_kategori' => $request->id_kategori,
             'nama_barang' => $request->nama_barang,
             'stok' => $request->stok,
