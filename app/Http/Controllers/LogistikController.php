9d044ba2a399a50bb2055ffd684eb314a77046a2
dika fajar <verdikaf@gmail.com>
2020-09-25 03:14:16 +0700

    fixed bug logistik

--- app/Http/Controllers/LogistikController.php
+++ app/Http/Controllers/LogistikController.php
@@ -14,7 +14,7 @@ class LogistikController extends Controller
         $logistik=DB::table('t_logistik')
         ->join('t_kategori_logistik','t_logistik.id_kategori','=','t_kategori_logistik.id')
         ->join('t_supplier','t_logistik.id_supplier','=','t_supplier.id')
-        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
+        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_logistik.id_kategori', 't_kategori_logistik.jenis_kategori','t_logistik.id_supplier', 't_supplier.nama')
         ->orderByDesc('id_logistik')
         ->paginate(3);
         
@@ -25,7 +25,7 @@ class LogistikController extends Controller
         $logistik=DB::table('t_logistik')
         ->join('t_kategori_logistik','t_logistik.id_kategori','=','t_kategori_logistik.id')
         ->join('t_supplier','t_logistik.id_supplier','=','t_supplier.id')
-        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_kategori_logistik.jenis_kategori', 't_supplier.nama')
+        ->select('t_logistik.id_logistik', 't_logistik.nama_barang', 't_logistik.stok', 't_logistik.status', 't_logistik.expired', 't_logistik.id_kategori', 't_kategori_logistik.jenis_kategori','t_logistik.id_supplier', 't_supplier.nama')
         ->where('t_logistik.id_logistik', '=', $id)
         ->get();
         
