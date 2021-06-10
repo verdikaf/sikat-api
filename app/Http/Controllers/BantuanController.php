64332038a48ab568430227e1b94b380d3a5ad25c
Verdika Fajar Saputra <verdikaf@gmail.com>
2021-06-10 19:47:23 +0700

    bantuan & tim

--- app/Http/Controllers/BantuanController.php
+++ app/Http/Controllers/BantuanController.php
@@ -6,5 +6,60 @@ use Illuminate\Http\Request;
 
 class BantuanController extends Controller
 {
-    //
+    public function index(){
+        return t_bantuan::query()->orderByDesc('id')->paginate(3);
+    }
+
+    public function showAll(){
+        return t_bantuan::all();
+    }
+
+    public function show($id){
+        return t_bantuan::find($id);
+    }
+
+    public function search(Request $request){
+
+        $q = $request->input('q');
+        $supplier = DB::table('t_bantuan')
+                    ->where('nama', 'like', '%' . $q . '%')
+                    ->paginate(3);
+
+        return Response::json($supplier);
+    }
+
+    public function store(Request $request){
+        $valid = Validator::make($request->all(), [
+            'nama' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        if($valid->fails()){
+            return response()->json(
+                ['error'=>$valid->errors()],
+                403
+            );
+        }
+
+        return t_bantuan::create($request->all());
+    }
+
+    public function update(Request $request, $id){
+        $valid = Validator::make($request->all(), [
+            'nama' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        if($valid->fails()){
+            return response()->json(
+                ['error'=>$valid->errors()],
+                403
+            );
+        }
+        
+        $supplier = t_bantuan::findOrFail($id);
+        $supplier->update($request->all());
+
+        return $supplier;
+    }
 }
